cf8109b1b476472ac4f1fb876ace85c52ff74787
Krzysztof Bienias <cyskowsky@gmail.com>
2022-03-15 19:15:41 +0100

    Created navigation

---
+++ src/components/Navigation/Navigation.styled.js
@@ -0,0 +1,49 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+  z-index: 10000;
+  width: 235px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: auto;
+  background-color: #eee;
+  position: absolute;
+  right: 0;
+  top: 50px;
+  transition: transform 0.5s ease-in-out;
+  transform: translateX(${({ isOpen }) => (isOpen ? '0' : '100%')});
+`;
+
+export const NavList = styled.ul`
+  padding: 0;
+  list-style: none;
+`;
+
+export const NavListItem = styled.li`
+  margin: 10px 0;
+`;
+
+const activeClassName = 'selected';
+export const StyledNavLink = styled(NavLink).attrs((props) => ({
+  tabIndex: props.isOpen ? null : '-1',
+  activeClassName,
+}))`
+  font-family: 'IBM Plex Mono', monospace;
+  text-decoration: none;
+  color: black;
+  font-size: 14px;
+  &.${activeClassName} {
+    font-weight: bold;
+  }
+`;
+
+export const NavButton = styled.button`
+  width: 40px;
+  height: 40px;
+  background-color: #eee;
+  border: none;
+  position: absolute;
+  left: -40px;
+`;
